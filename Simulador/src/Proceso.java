27cf821069e5e3096b31d511977803cbb328b00c
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-03-02 00:12:31 -0600

    Creación de clase Proceso
    Se desarrolló la clase proceso que contiene los atributos de tamaño para la creación de label y tiempo de proceso

---
+++ Simulador/src/Proceso.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author HP15DA0023LA
+ */
+public class Proceso extends JLabel{
+    private Integer tamaño, tiempo, min, max;
+    private String hexadecimal;
+
+    public Proceso(Integer min) {
+        // los procesos van de 1 ms a 20 ms
+        this.tiempo = (int)(Math.random()*20.0+1.0);
+        this.tamaño = tiempo*50; // el tamaño va a ser en MB y el maximo de la memoria principal es de 4 GB
+        this.hexadecimal= Integer.toHexString(this.tamaño);
+        this.min=min;
+        this.max=min+tamaño;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public String getHexadecimal() {
+        return hexadecimal;
+    }
+    public boolean reducir(){
+        boolean seAcabo=true;
+        tiempo-=3;
+        if(tiempo>0) seAcabo=false;
+        return seAcabo;
+    }
+
+    public Integer getTiempo() {
+        return tiempo;
+    }
+
+    public Integer getTamaño() {
+        return tamaño;
+    }
+    public String getMinComoHexa() {
+        return Integer.toHexString(this.min);
+    }
+    public String getMaxComoHexa() {
+        return Integer.toHexString(this.max);
+    }
+    public Integer getTamañoEnPix() {
+        return (tamaño*250/4096);
+    }
+}
